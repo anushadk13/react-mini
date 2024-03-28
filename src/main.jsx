2cf206de55220adaa16193ab828f5a16d894f93a
anushadk13 <122234698+anushadk13@users.noreply.github.com>
2024-03-28 23:46:37 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Week1 from './Week1'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <Week1/>
   </React.StrictMode>,
 )
